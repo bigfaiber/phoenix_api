3d05ad2b1dac169467825149bba947936629524d
Andres Gutierrez <agutierrezt@unal.edu.co>
2017-12-14 18:59:36 -0500

    Deploy set up We have used capistrano in order to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,55 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'projects/:id/amortization-table', to: "projects#generate_table", defaults: { format: 'pdf' }
-  scope '/', defaults: { format: 'json' } do
-    post '/login',  to: "authenticate#login"
-    resources :clients do
-      collection do
-        get 'new-clients', to: "clients#new_clients"
-        get 'old-clients', to: "clients#old_clients"
-        get 'verification', to: "clients#verification"
-        post 'upload-avatar', to: "clients#avatar"
-        post 'create-goods', to: "clients#goods"
-        post 'upload-document', to: "clients#documents"
-      end
-    end
-    resources :investors do
-      resources :projects, only: [] do
-        member do
-          post 'match', to: "projects#match"
+  constraints subdomain: "api" do
+    get 'projects/:id/amortization-table', to: "projects#generate_table", defaults: { format: 'pdf' }
+    scope '/', defaults: { format: 'json' } do
+      post '/login',  to: "authenticate#login"
+      resources :clients do
+        collection do
+          get 'new-clients', to: "clients#new_clients"
+          get 'old-clients', to: "clients#old_clients"
+          get 'verification', to: "clients#verification"
+          post 'upload-avatar', to: "clients#avatar"
+          post 'create-goods', to: "clients#goods"
+          post 'upload-document', to: "clients#documents"
         end
       end
-      collection do
-        get 'new-investors', to: "investors#new_investors"
-        get 'old-investors', to: "investors#old_investors"
-        get 'verification', to: "investors#verification"
-        post 'upload-avatar', to: "investors#avatar"
-        post 'create-payment', to: "investors#payment"
-        post 'upload-document', to: "investors#documents"
+      resources :investors do
+        resources :projects, only: [] do
+          member do
+            post 'match', to: "projects#match"
+          end
+        end
+        collection do
+          get 'new-investors', to: "investors#new_investors"
+          get 'old-investors', to: "investors#old_investors"
+          get 'verification', to: "investors#verification"
+          post 'upload-avatar', to: "investors#avatar"
+          post 'create-payment', to: "investors#payment"
+          post 'upload-document', to: "investors#documents"
+        end
       end
+      resources :admins do
+        collection do
+          post 'upload-avatar', to: "admins#avatar"
+        end
+      end
+      resources :projects, only: [:create,:update,:destroy,:show,:index] do
+        collection do
+          get 'by-clients', to: "projects#clients"
+          get 'by-investors', to: "projects#investors"
+          get 'search', to: "projects#search"
+        end
+        member do
+          post 'change-interest', to: "projects#rate"
+          post 'add-account', to: "projects#account"
+          post 'approve-project', to: "projects#approve"
+          post 'like', to: "projects#like"
+          post 'add-receipt', to: "projects#receipt"
+        end
+      end
+      resources :matches, only: [:index]
     end
-    resources :admins do
-      collection do
-        post 'upload-avatar', to: "admins#avatar"
-      end
-    end
-    resources :projects, only: [:create,:update,:destroy,:show,:index] do
-      collection do
-        get 'by-clients', to: "projects#clients"
-        get 'by-investors', to: "projects#investors"
-        get 'search', to: "projects#search"
-      end
-      member do
-        post 'change-interest', to: "projects#rate"
-        post 'add-account', to: "projects#account"
-        post 'approve-project', to: "projects#approve"
-        post 'like', to: "projects#like"
-        post 'add-receipt', to: "projects#receipt"
-      end
-    end
-    resources :matches, only: [:index]
   end
 
 end
