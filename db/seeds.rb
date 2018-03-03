b61e34fa155d5389899242d4db0e3a71962ab0de
Andres Gutierrez <agutierrezt@slabcode.com>
2018-03-03 13:37:08 -0500

    Admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-a = Admin.new(name: 'Admin', lastname: 'Phoenix', email: 'admin@phx.com.co',password: '0123456789', password_confirmation: '0123456789')
+a = Admin.new(name: 'Carlos', lastname: 'Phoenix', email: 'carlos@phx.com.co',password: 'Tikeromm1', password_confirmation: 'Tikeromm1')
 a.save
+b = Admin.new(name: 'Andres', lastname: 'Gutierrez', email: "agutierrezt@slabcode.com", password: 'Gutierrez1003739139',password_confirmation:'Gutierrez1003739139')
+b.save
