ea67d04b8108c1b200ba9f18fb336272bb6012c9
Andres Gutierrez <agutierrezt@slabcode.com>
2018-07-16 22:39:20 -0500

    Correction for the migration of match for projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180717031936) do
+ActiveRecord::Schema.define(version: 20180717033028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
