60b298fd70145ef27b1874a0dadf76fec0fc4820
Andres Gutierrez <agutierrezt@slabcode.com>
2018-08-28 16:20:03 -0500

    Small changes

--- app/models/inv_account.rb
+++ app/models/inv_account.rb
@@ -14,7 +14,7 @@ class InvAccount < ApplicationRecord
   validates_length_of :account_number, minimum: 6, maximum: 20
 
   def self.load(page: 1, per_page: 10)
-    paginate(page: 1, per_page: 10)
+    paginate(page: page, per_page: per_page)
   end
 
   def self.by_id(id)
