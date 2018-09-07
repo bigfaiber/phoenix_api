a3ee33f24c618f657a5f65d79b40413ffba371d0
Andres Gutierrez <agutierrezt@slabcode.com>
2018-09-07 16:17:20 -0500

    Corrections

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,7 @@ class StaticController < ApplicationController
 
   def average_interest
     average =  Project.average_interest
+    average ||= 1.5
     render json: {
       data: {
         interest: average
