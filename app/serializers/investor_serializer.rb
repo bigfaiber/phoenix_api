98f03145740d2aaa07217abe84bffc01a1a00e77
Andres Gutierrez <agutierrezt@slabcode.com>
2018-11-26 20:47:57 -0500

    Small changes

--- app/serializers/investor_serializer.rb
+++ app/serializers/investor_serializer.rb
@@ -1,5 +1,5 @@
 class InvestorSerializer < ActiveModel::Serializer
-  attributes :id,:step,:name,:lastname,:identification,:phone,:address,:code_confirmation,:birthday,:email,:city,:employment_status,:education,:rent_tax,:avatar,:money_invest,:month,:monthly_payment,:profitability,:maximum,:career
+  attributes :id,:step,:name,:lastname,:identification,:phone,:address,:code_confirmation,:birthday,:email,:city,:employment_status,:education,:rent_tax,:avatar,:money_invest,:month,:monthly_payment,:profitability,:maximum,:career,:technical_career
   attribute :created_at
   attribute :debt
   has_one :payment
