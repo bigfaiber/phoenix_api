abb0c960381d452a30b40251eb2d1f1fc6bee9b0
bigfaiber <greatfaiber@gmail.com>
2019-10-02 12:03:42 -0500

    specs for staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,15 +4,15 @@ lock "3.10.1"
 #Testing
 #server '35.231.77.182', port: 22, roles: [:web, :app, :db], primary: true
 #Production
-server '35.196.49.244', port: 22, roles: [:web, :app, :db], primary: true
+server '35.231.77.182', port: 22, roles: [:web, :app, :db], primary: true
 
 
-set :repo_url,        'git@bitbucket.org:aliCamargo/phx_api.git'
+set :repo_url,        'git@github.com:bigfaiber/phoenix_api.git'
 set :application,     'api'
 #Testing
 #set :user,            'AndresGutierrez'
 #Production
-set :user,            'phx'
+set :user,            'faiber'
 set :puma_threads,    [4, 16]
 set :puma_workers,    2   
 set :migration_role, :db
@@ -29,7 +29,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"    
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/phx-user) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.Keys/faiber-phx) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -93,4 +93,4 @@ end
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
-#sudo ln -nfs /home/phx/apps/api/current/config/nginx.conf /etc/nginx/conf.d/api.conf
+#sudo ln -nfs /home/faiber/apps/api/current/config/nginx.conf /etc/nginx/conf.d/api.conf
