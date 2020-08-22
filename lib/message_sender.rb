67f1fd7de351f62e90985a57b6d3a3dbaf48e9fe
bigfaiber <greatfaiber@gmail.com>
2020-08-22 16:22:09 -0500

    secure twilio credentials

--- lib/message_sender.rb
+++ lib/message_sender.rb
@@ -4,14 +4,14 @@ class MessageSender
   end
 
   def initialize
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token  = ENV['TWILIO_AUTH_TOKEN']
+    account_sid = system("curl 'http://metadata.google.internal/computeMetadata/v1/project/attributes/TWILIO_ACCOUNT_SID' -H 'Metadata-Flavor: Google'")
+    auth_token  = system("curl 'http://metadata.google.internal/computeMetadata/v1/project/attributes/TWILIO_AUTH_TOKEN' -H 'Metadata-Flavor: Google'")
     @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
   def send_message(code,phone)
     @client.messages.create(
-      from:  ENV['SENDER_PHONE'],
+      from:  system("curl 'http://metadata.google.internal/computeMetadata/v1/project/attributes/SENDER_PHONE' -H 'Metadata-Flavor: Google'"),
       to:    phone,
       body:  "Su codigo de verificacon es: \n #{code}"
     )
