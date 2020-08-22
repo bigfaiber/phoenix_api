e239f883e9791689880b83b7054c535e210c3461
bigfaiber <greatfaiber@gmail.com>
2020-08-22 15:56:01 -0500

    metadata with curl

--- config/initializers/mandrill.rb
+++ config/initializers/mandrill.rb
@@ -1,4 +1,6 @@
 require 'mandrill'
 
 # Use an environment variable instead of placing the key in source code
-MANDRILL = Mandrill::API.new(ENV['MAILCHIMP_KEY'])
+MANDRILL = Mandrill::API.new(system"curl 'http://metadata.google.internal/computeMetadata/v1/project/attributes/MAILCHIMP_KEY' -H 'Metadata-Flavor: Google'")
+
+
