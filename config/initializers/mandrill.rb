183ef658dfcc37411581e71d9a80bd6b426631b5
bigfaiber <greatfaiber@gmail.com>
2020-08-23 11:18:20 -0500

    gcloude load

--- config/initializers/mandrill.rb
+++ config/initializers/mandrill.rb
@@ -3,5 +3,5 @@ require 'mandrill'
 # Use an environment variable instead of placing the key in source code
 # url = 'http://metadata.google.internal/computeMetadata/v1/project/attributes/MAILCHIMP_KEY'
 # header = {"Metadata-Flavor": "Google"}
-
+CloudEnvImporter.new.add_cloud_vars_to_env
 MANDRILL = Mandrill::API.new(ENV['MAILCHIMP_KEY'])
