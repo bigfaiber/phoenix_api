478f5e7694791439629a8c532f41eee6b5ddc4b3
bigfaiber <greatfaiber@gmail.com>
2020-08-23 12:21:44 -0500

    fix more exceptions

--- lib/cloud_env_importer.rb
+++ lib/cloud_env_importer.rb
@@ -7,21 +7,20 @@ class CloudEnvImporter
     begin
       self.class.get(service_url, {headers: headers})
     rescue
-      p "I'm on Heroku Environment"
+      nil
     end
   end
 
   def get_env_vars
-    keys = get_request.parsed_response.split("\n") - ['ssh-keys']
+    keys = get_request&.parsed_response.split("\n") - ['ssh-keys']
     env_vars = {}
-    keys.each do |key|
-      env_vars[key] = get_request("/#{key}").parsed_response
-    end
+    keys.each{ |key| env_vars[key] = get_request("/#{key}")&.parsed_response} if keys
     env_vars
   end
 
   def add_cloud_vars_to_env
-    ENV.update get_env_vars
+    gcloud_metadata = get_env_vars
+    ENV.update(gcloud_metadata) if gcloud_metadata
   end
 
   # def get_twilio_sid
