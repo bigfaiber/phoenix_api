4ad15ea30ab834717ad5097d853e7f0a9a98b446
Mauricio Scioville <invasormou@gmail.com>
2020-10-31 15:55:15 -0500

    if interest 0 change to 1.5

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,9 @@
 class StaticController < ApplicationController
 
   def average_interest
-    if Project.all.size > 0
-      average =  Project.average_interest
+    average =  Project.average_interest
+    if average == 0
+      average = 1.5
     end
     average ||= 1.5
     render json: {
